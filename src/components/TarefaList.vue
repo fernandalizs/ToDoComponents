3d511476a021180747a6224c5a36434c4ad3e774
Fernanda Liz <fernandaliz2004@hotmail.com>
2022-09-08 14:56:49 -0300

    first commit

---
+++ src/components/TarefaList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">{{ task }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TarefaList",
+  props: {
+    msg: String,
+    tasks: Array,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <TarefaList msg="Welcome to Your Vue.js App" :tasks="listaDeTarefas" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import TarefaList from "../components/TarefaList.vue";
+
+export default {
+  components: {
+    TarefaList,
+  },
+  data: () => {
+    return {
+      listaDeTarefas: ["A", "B", "C"],
+    };
+  },
+  created() {
+    console.log("terminei de carregar a pagina");
+    axios.get("http://localhost:3000/tasks/").then((response) => {
+      console.log(response.data);
+      this.listaDeTarefas = response.data;
+    });
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
